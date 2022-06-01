838e81c141df0c23de8f76183089819622b09f39
valero3000 <104862017+valero3000@users.noreply.github.com>
2022-06-01 01:44:06 -0400

    Add files via upload

---
+++ ProyectoPaginaJuegos/Diseño/Boleta.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPaginaJuegos.Diseño
+{
+    internal class Boleta
+    {
+        private String id;
+        private String id_pro;
+        private String id_cliente;
+
+        public string Id { get => id; set => id = value; }
+        public string Id_pro { get => id_pro; set => id_pro = value; }
+        public string Id_cliente { get => id_cliente; set => id_cliente = value; }
+
+        public Boleta()
+        {
+        }
+
+        public Boleta(string id, string id_pro, string id_cliente)
+        {
+            Id = id;
+            Id_pro = id_pro;
+            Id_cliente = id_cliente;
+           
+        }
+    }
+}
---
+++ ProyectoPaginaJuegos/Diseño/Producto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPaginaJuegos.Diseño
+{
+    internal class Producto
+    {
+        private String precio_pro;
+        private String id_pro;
+        private String nombre_pro;
+
+        public string Precio_pro { get => precio_pro; set => precio_pro = value; }
+        public string Id_pro { get => id_pro; set => id_pro = value; }
+        public string Nombre_pro { get => nombre_pro; set => nombre_pro = value; }
+
+        public Producto()
+        {
+        }
+
+        public Producto(string precio_pro, string id_pro, string nombre_pro)
+        {
+            Precio_pro = precio_pro;
+            Id_pro = id_pro;
+            Nombre_pro = nombre_pro;
+            
+        }
+    }
+}
